9b3a2f9ea58a4a0d1c671eb1b5d9ed902a0bfd37
arina <arinazav02@gmail.com>
2026-07-08 23:39:32 +0300

    Added DTO for User class: UserResponse, UserRequest

--- src/main/java/com/example/ToDoTask/dto/request/UserRequest.java
+++ src/main/java/com/example/ToDoTask/dto/request/UserRequest.java
@@ -1,4 +1,34 @@
 package com.example.ToDoTask.dto.request;
 
+import com.example.ToDoTask.enums.Role;
+import com.example.ToDoTask.model.Task;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRequest {
+
+    @NotBlank
+    @Size(min = 3)
+    private String username;
+
+    @Email
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Почта невалидна"
+    )
+    private String email;
+
+    @Size(min = 8)
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private Role role;
+
 }
--- src/main/java/com/example/ToDoTask/dto/response/UserResponse.java
+++ src/main/java/com/example/ToDoTask/dto/response/UserResponse.java
@@ -2,23 +2,20 @@ package com.example.ToDoTask.dto.response;
 
 import com.example.ToDoTask.enums.Role;
 import com.example.ToDoTask.enums.Status;
+import com.example.ToDoTask.model.Task;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class UserResponse {
-    @NotNull
-    Long id;
-    @NotNull
-    @NotBlank
-    @Length(min = 2)
-    String title;
-    @NotBlank
-    String description;
-    Status status;
-    Role role;
-    LocalDateTime createdAt;
-    LocalDateTime deadline;
+    private Long id;
+    private String username;
+    private String email;
+    private Role role;
+    private List<Task> tasksByAuthor;
+    private List<Task> tasksByAssignee;
+
 }
